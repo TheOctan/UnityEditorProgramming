9687d8ba9412d5e39ed2adb5f281e56a7cc90b0a
Kanatush Timofey <kanatush.tim@gmail.com>
2023-01-27 15:30:37 +0300

    Scriptable object monster data

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,19 +1,14 @@
 using UnityEngine;
 
+[SelectionBase]
 public class Monster : MonoBehaviour
 {
-    [Header("General stats")]
-    [SerializeField] private string _name;
-    [SerializeField] private MonsterType _monsterType;
-    [SerializeField, Range(0, 100)] private float _chanceToDropItem = 50f;
-    [Tooltip("Radius size where monster will see the player")]
-    [SerializeField] private float _rangeOfAwareness;
+    [SerializeField] private MonsterData _data;
 
-    [Header("Combat stats")]
-    [SerializeField] private int _damage;
-    [SerializeField] private int _health;
-    [SerializeField] private int _speed;
+    public MonsterData Data => _data;
 
-    [Header("Dialogue")]
-    [SerializeField, TextArea] private string _battleCry;
+    private void Awake()
+    {
+        Debug.Log($"Name {_data.Name}\nDamage {_data.Damage}");
+    }
 }
