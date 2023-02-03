9c9ce32c020d7ccc99d2a635688b71144add6b1d
Kanatush Timofey <kanatush.tim@gmail.com>
2023-02-03 12:53:42 +0300

    Help box for monster data

--- Assets/Editor/MonsterDataEditor.cs
+++ Assets/Editor/MonsterDataEditor.cs
@@ -14,6 +14,20 @@ public class MonsterDataEditor : Editor
         DrawProgressBar(difficulty / 100, "Difficulty");
 
         base.OnInspectorGUI();
+
+        if (data.Name == string.Empty)
+        {
+            EditorGUILayout.HelpBox(
+                "Caution: No name specified. Please name the monster!", MessageType.Warning);
+        }
+        if (data.Type == MonsterType.Undefined)
+        {
+            EditorGUILayout.HelpBox("No Monster type selected", MessageType.Warning);
+        }
+        if (data.Health < 0)
+        {
+            EditorGUILayout.HelpBox("Should not have negative Health", MessageType.Warning);
+        }
     }
 
     private static void DrawProgressBar(float value, string label)
