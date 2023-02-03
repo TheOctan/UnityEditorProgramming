07794e86f7ce3d898b639bbc59bac5e87874c932
Kanatush Timofey <kanatush.tim@gmail.com>
2023-02-03 12:14:41 +0300

    Custom editor for `MonsterData`

---
+++ Assets/Editor/MonsterDataEditor.cs
@@ -0,0 +1,14 @@
+﻿using OctanGames.MonsterMaker;
+using UnityEditor;
+
+[CustomEditor(typeof(MonsterData))]
+public class MonsterDataEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var data = target as MonsterData;
+
+        if (data is not null) EditorGUILayout.LabelField(data.Name.ToUpper(), EditorStyles.boldLabel);
+        base.OnInspectorGUI();
+    }
+}
