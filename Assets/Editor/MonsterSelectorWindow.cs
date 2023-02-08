3546ae267c0f1107d033be1995e8baec94014385
Kanatush Timofey <kanatush.tim@gmail.com>
2023-02-08 13:28:27 +0300

    Test Monster selector editor window

---
+++ Assets/Editor/MonsterSelectorWindow.cs
@@ -0,0 +1,25 @@
+﻿using OctanGames.MonsterMaker;
+using UnityEditor;
+using UnityEngine;
+
+namespace OctanGames
+{
+    public class MonsterSelectorWindow : EditorWindow
+    {
+        private MonsterType _selectedMonsterType = MonsterType.Undefined;
+        
+        [MenuItem("Window/Monster Selector")]
+        private static void ShowWindow()
+        {
+            GetWindow<MonsterSelectorWindow>("Monster Selector");
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.Space(10);
+            GUILayout.Label("Selection Filters:", EditorStyles.boldLabel);
+            _selectedMonsterType =
+                (MonsterType)EditorGUILayout.EnumPopup("Monster type to select:", _selectedMonsterType);
+        }
+    }
+}
